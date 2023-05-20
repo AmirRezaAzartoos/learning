224673c5cf116a7447c08b00ac51e9d41f2ca603
ar.azartoos <AmirRezaAzartoos@gmail.com>
2023-05-20 12:59:43 +0430

    chaper 19 final

--- WebDevelopment/Calculator/calculator.js
+++ WebDevelopment/Calculator/calculator.js
@@ -4,6 +4,10 @@ const bodyParser = require("body-parser")
 const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
 
+app.listen(3000, function(){
+    console.log("listenin on port 3000")
+})
+
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html")
 })
@@ -12,6 +16,10 @@ app.post("/", function(req, res){
     res.send("Your answet is " + (Number(req.body.num1) + Number(req.body.num2)))
 })
 
-app.listen(3000, function(){
-    console.log("listenin on port 3000")
-})  
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
+app.post("/bmicalculator", function(req, res){
+    res.send("Your BMI is " + (Number(req.body.weight) / (Number(req.body.height) ** 2)))
+})
