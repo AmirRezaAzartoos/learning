b75d440d1b8acc422f4b12f64cb28bb423cb4686
ar.azartoos <AmirRezaAzartoos@gmail.com>
2023-06-13 13:38:28 +0330

    start of section 32

---
+++ WebDevelopment/32-Secrets/app.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 3012;
+app.listen(PORT, function () {
+	console.log("Server is running on port ", PORT);
+});
+
+mongoose
+	.connect("mongodb://freedom.dementor.info:27017/userDB", {
+		auth: {
+			username: "Dem_Mongo",
+			password: "Mongo1998",
+		},
+		authSource: "admin",
+	})
+	.then(function (ack) {
+		console.log("Successfuly connected to database: ");
+	})
+	.catch(function (err) {
+		console.log("Failed to connect: ", err);
+	});
+
+const userSchema = mongoose.Schema({
+	email: String,
+	password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function (req, res) {
+	res.render("home");
+});
+
+app.route("/register")
+	.get(function (req, res) {
+		res.render("register");
+	})
+	.post(function (req, res) {
+		User({
+			email: req.body.username,
+			password: req.body.password,
+		})
+			.save()
+			.then(function (ack) {
+				console.log("User saved successfully.");
+				res.render("secrets");
+			})
+			.catch(function (err) {
+				console.log(err);
+			});
+	});
+
+app.route("/login")
+	.get(function (req, res) {
+		res.render("login");
+	})
+	.post(function (req, res) {
+		User.findOne({
+			email: req.body.username,
+		})
+			.exec()
+			.then(function (ack) {
+				if (ack.password === req.body.password) res.render("secrets");
+			})
+			.catch(function (err) {
+				console.log(err);
+			}).finally(function (param) {  })
+	});
